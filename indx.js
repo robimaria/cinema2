2b7fa857b9b1129861745e5910731da8e0c8689f
robimaria <143037413+robimaria@users.noreply.github.com>
2023-11-08 18:16:32 -0300

    Update indx.js

--- indx.js
+++ indx.js
@@ -88,8 +88,99 @@ const movies = [
         releaseDate: "1 de Dezembro, 2023",
         price: 10.99,
         availableTickets: 100
+    },
+    {
+        name: "O Filme 11",
+        image: "https://uploads.jovemnerd.com.br/wp-content/uploads/2022/12/aranhaverso_2_poster__2220u0-760x950.jpg",
+        date: "20 de Dezembro, 2023",
+        inTheaters: true,
+        releaseDate: "5 de Dezembro, 2023",
+        price: 12.99,
+        availableTickets: 150
+    },
+    {
+        name: "O Filme 12",
+        image: "https://upload.wikimedia.org/wikipedia/pt/8/8d/The_Wolf_of_Wall_Street.jpg",
+        date: "25 de Dezembro, 2023",
+        inTheaters: true,
+        releaseDate: "10 de Dezembro, 2023",
+        price: 10.99,
+        availableTickets: 100
+    },
+    {
+        name: "O Filme 13",
+        image: "https://br.web.img3.acsta.net/c_310_420/pictures/21/04/07/21/13/5564614.jpg",
+        date: "30 de Dezembro, 2023",
+        inTheaters: true,
+        releaseDate: "15 de Dezembro, 2023",
+        price: 11.99,
+        availableTickets: 120
+    },
+    {
+        name: "O Filme 14",
+        image: "https://br.web.img2.acsta.net/pictures/21/12/06/21/17/3973076.jpg",
+        date: "5 de Janeiro, 2024",
+        inTheaters: true,
+        releaseDate: "20 de Dezembro, 2023",
+        price: 9.99,
+        availableTickets: 90
+    },
+    {
+        name: "O Filme 15",
+        image: "https://br.web.img3.acsta.net/pictures/14/05/27/15/43/549754.jpg",
+        date: "10 de Janeiro, 2024",
+        inTheaters: true,
+        releaseDate: "25 de Dezembro, 2023",
+        price: 12.99,
+        availableTickets: 150
+    },
+    {
+        name: "O Filme 16",
+        image: "https://br.web.img3.acsta.net/c_310_420/pictures/14/06/12/15/31/228587.jpg",
+        date: "15 de Janeiro, 2024",
+        inTheaters: true,
+        releaseDate: "1 de Janeiro, 2024",
+        price: 10.99,
+        availableTickets: 100
+    },
+    {
+        name: "O Filme 17",
+        image: "https://upload.wikimedia.org/wikipedia/pt/6/66/Cherry_2021.jpg",
+        date: "20 de Janeiro, 2024",
+        inTheaters: true,
+        releaseDate: "5 de Janeiro, 2024",
+        price: 11.99,
+        availableTickets: 120
+    },
+    {
+        name: "O Filme 18",
+        image: "https://upload.wikimedia.org/wikipedia/pt/thumb/9/9b/Avengers_Endgame.jpg/250px-Avengers_Endgame.jpg",
+        date: "25 de Janeiro, 2024",
+        inTheaters: true,
+        releaseDate: "10 de Janeiro, 2024",
+        price: 9.99,
+        availableTickets: 90
+    },
+    {
+        name: "O Filme 19",
+        image: "https://upload.wikimedia.org/wikipedia/pt/a/af/Fantastic_mr_fox.jpg",
+        date: "30 de Janeiro, 2024",
+        inTheaters: true,
+        releaseDate: "15 de Janeiro, 2024",
+        price: 12.99,
+        availableTickets: 150
+    },
+    {
+        name: "O Filme 20",
+        image: "https://br.web.img2.acsta.net/pictures/21/10/05/10/14/4942486.jpg",
+        date: "5 de Fevereiro, 2024",
+        inTheaters: true,
+        releaseDate: "20 de Janeiro, 2024",
+        price: 10.99,
+        availableTickets: 100
     }
 ];
+
 
 const movieList = document.getElementById("movie-list");
 
